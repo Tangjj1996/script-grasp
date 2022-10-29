eb7efcbfb9d9a796b0478268926eb254ef33bae3
Tangjj1996 <awesometangji@gmail.com>
2022-10-29 18:32:17 +0800

    feat: add for img load

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import puppeteer from "puppeteer";
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, devtools: false });
   const page = await browser.newPage();
-  await page.goto("https://www.yuque.com/weijin_is_wiki/ykf0s9/fxbfgc");
+  await page.goto("https://www.yuque.com/weijin_is_wiki/ykf0s9/fxbfgc", {
+    waitUntil: "networkidle0",
+  });
+  await page.evaluate(async () => {
+    const { scrollHeight } = document.body;
+    const STEP = 1000;
+
+    let n = Math.ceil(scrollHeight / STEP);
+    while (n--) {
+      window.scrollBy(0, STEP);
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve(true);
+        }, 1000)
+      );
+    }
+  });
   await page.pdf({ path: "dist/1.pdf" });
   await browser.close();
 }
